9ea7c383fc5f69e31602d7eb6c5f9aa29f5eab3e
KotenkoNata <kotenkonata8@gmail.com>
2023-11-28 14:57:11 +0100

    feat: install react native Skia, create pathes for HomePage

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import {useFonts, Roboto_500Medium} from "@expo-google-fonts/roboto";
 import HomeScreen from "./screens/HomeScreen";
 
@@ -7,28 +5,12 @@ export default function App() {
  let [fontsLoaded] = useFonts({
    Roboto_500Medium,
  })
-
     if (!fontsLoaded) {
         return null;
     }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="black" />
-    </View>
+      <HomeScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    fontFamily: 'Roboto_500Medium',
-      fontSize: 20,
-  }
-});
