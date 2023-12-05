9f4d80c345e9fa1bde21313a7c9d62241deccfcb
KotenkoNata <kotenkonata8@gmail.com>
2023-12-05 15:58:08 +0100

    feat: correct svg size for onboarding

--- data/onboarding-data.js
+++ data/onboarding-data.js
@@ -1,20 +1,20 @@
 const onboardingData=[{
     id: '1',
-    image: require('../assets/img/Screenshot1.svg'),
+    image:'',
     title: 'Order ingredients',
     subtitle: 'Order the ingredients you need quickly with a fast process',
     buttonName: 'Next'
 },
     {
     id: '2',
-        image: require('../assets/img/Screenshot1.svg'),
+        image: '',
     title: `Let's cooking`,
     subtitle: 'Cooking based on the food recipes you find and the food you love',
     buttonName: 'Next'
 },
     {
         id: '3',
-        image: require('../assets/img/Screenshot1.svg'),
+        image: '',
         title: `All recipe you need`,
         subtitle: '5000+ healthy recipes made by people for your healthy life',
         buttonName: 'Get Started'
